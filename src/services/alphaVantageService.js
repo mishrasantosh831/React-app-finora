bc48e85e2864597fedc8f5b6991af3de1c1e145a
Santosh <mishrasantosh831@gmail.com>
2026-03-25 10:39:08 +0530

    Initial Push

---
+++ src/services/alphaVantageService.js
@@ -0,0 +1,37 @@
+const API_KEY = import.meta.env.VITE_ALPHAVANTAGE_API_KEY;
+const BASE_URL = 'https://www.alphavantage.co/query?';
+
+if(!API_KEY) {
+  throw new Error('Missing API key. Please set the VITE_ALPHAVANTAGE_API_KEY environment variable.')
+}
+
+async function fetchJSON(queryParam){
+    const url = `${BASE_URL}${new URLSearchParams({
+        ...queryParam,
+        apikey: API_KEY,
+    })}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+
+export async function getDailyPerformance(ticker) {
+    const json = await fetchJSON({
+        function: 'TIME_SERIES_DAILY',
+        symbol: ticker,
+    });
+    console.log(json);
+    const timeSeries = json['Time Series (Daily)'];
+    if(!timeSeries) return null;
+    const [latestDate, previousDate] = Object.keys(timeSeries);
+
+    if(!latestDate || !previousDate) return null;
+
+    const latestClose = parseFloat(timeSeries[latestDate]['4. close']);
+    const previousClose = parseFloat(timeSeries[previousDate]['4. close']);
+
+    const performance = ((latestClose / previousClose - 1) * 100).toFixed(2);
+    return performance;
+}
